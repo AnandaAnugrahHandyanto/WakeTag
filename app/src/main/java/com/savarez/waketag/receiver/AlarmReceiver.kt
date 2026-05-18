8b6bed8683511d270615611e63710341e04a6bb6
AnandaAnugrahHandyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>
2026-05-18 17:29:09 +0700

    feat: register alarm broadcast receiver

---
+++ app/src/main/java/com/savarez/waketag/receiver/AlarmReceiver.kt
@@ -0,0 +1,14 @@
+package com.savarez.waketag.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("WakeTag", "Alarm Triggered")
+    }
+}
+
